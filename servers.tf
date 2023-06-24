a2902591ffb7e34a6fc3f2239712f76973749b16
Deepika <venkatadeepikathulasi@gmail.com>
2023-06-24 18:47:23 +0530

    create terraform instances for all components

--- servers.tf
+++ servers.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "frontend" {
-  ami           = ami-03265a0778a880afb
+  ami           = "ami-03265a0778a880afb"
   instance_type = "t3.micro"
 
   tags = {
@@ -7,7 +7,7 @@ resource "aws_instance" "frontend" {
   }
 }
 resource "aws_instance" "mongodb" {
-  ami           = ami-03265a0778a880afb
+  ami           = "ami-03265a0778a880afb"
   instance_type = "t3.micro"
 
   tags = {
@@ -16,7 +16,7 @@ resource "aws_instance" "mongodb" {
 }
 
 resource "aws_instance" "catalogue" {
-  ami           = ami-03265a0778a880afb
+  ami           = "ami-03265a0778a880afb"
   instance_type = "t3.micro"
 
   tags = {
@@ -24,7 +24,7 @@ resource "aws_instance" "catalogue" {
   }
 }
 resource "aws_instance" "redis" {
-  ami           = ami-03265a0778a880afb
+  ami           = "ami-03265a0778a880afb"
   instance_type = "t3.micro"
 
   tags = {
@@ -32,7 +32,7 @@ resource "aws_instance" "redis" {
   }
 }
 resource "aws_instance" "cart" {
-  ami           = ami-03265a0778a880afb
+  ami           = "ami-03265a0778a880afb"
   instance_type = "t3.micro"
 
   tags = {
@@ -40,7 +40,7 @@ resource "aws_instance" "cart" {
   }
 }
 resource "aws_instance" "mysql" {
-  ami           = ami-03265a0778a880afb
+  ami           = "ami-03265a0778a880afb"
   instance_type = "t3.micro"
 
   tags = {
@@ -48,7 +48,7 @@ resource "aws_instance" "mysql" {
   }
 }
 resource "aws_instance" "shipping" {
-  ami           = ami-03265a0778a880afb
+  ami           = "ami-03265a0778a880afb"
   instance_type = "t3.micro"
 
   tags = {
@@ -56,7 +56,7 @@ resource "aws_instance" "shipping" {
   }
 }
 resource "aws_instance" "rabbitmq" {
-  ami           = ami-03265a0778a880afb
+  ami           = "ami-03265a0778a880afb"
   instance_type = "t3.micro"
 
   tags = {
