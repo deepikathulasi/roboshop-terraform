b71d47b0f03fb52d5afc596323cf3152bf7f249b
Deepika <venkatadeepikathulasi@gmail.com>
2023-06-25 07:30:11 +0530

    create terraform instances for all components

--- servers.tf
+++ servers.tf
@@ -20,6 +20,14 @@ output "frontend"{
 value = aws_instance.frontend.public_ip
 }
 
+resource "aws_route53_record" "frontend" {
+  zone_id = "Z0458836EHR8MPWSFHZ5"
+  name    = "frontend.devops1008.online"
+  type    = "A"
+  ttl     = "30"
+  records = [aws_instance.frontend.private_ip]
+}
+
 
 
 resource "aws_instance" "mongodb" {
@@ -33,6 +41,13 @@ resource "aws_instance" "mongodb" {
 output "mongodb"{
 value = aws_instance.mongodb.public_ip
 }
+resource "aws_route53_record" "mongodb" {
+  zone_id = "Z0458836EHR8MPWSFHZ5"
+  name    = "mongodb.devops1008.online"
+  type    = "A"
+  ttl     = "30"
+  records = [aws_instance.mongodb.private_ip]
+}
 
 resource "aws_instance" "catalogue" {
   ami           = data.aws_ami.centos.image_id
@@ -44,6 +59,13 @@ resource "aws_instance" "catalogue" {
 }
 output "catalogue"{
 value = aws_instance.catalogue.public_ip
+}
+resource "aws_route53_record" "catalogue" {
+  zone_id = "Z0458836EHR8MPWSFHZ5"
+  name    = "catalogue.devops1008.online"
+  type    = "A"
+  ttl     = "30"
+  records = [aws_instance.catalogue.private_ip]
 }
 
 resource "aws_instance" "redis" {
@@ -57,6 +79,13 @@ resource "aws_instance" "redis" {
 output "redis"{
 value = aws_instance.redis.public_ip
 }
+resource "aws_route53_record" "redis" {
+  zone_id = "Z0458836EHR8MPWSFHZ5"
+  name    = "redis.devops1008.online"
+  type    = "A"
+  ttl     = "30"
+  records = [aws_instance.redis.private_ip]
+}
 
 resource "aws_instance" "cart" {
   ami           = data.aws_ami.centos.image_id
@@ -68,6 +97,13 @@ resource "aws_instance" "cart" {
 }
 output "cart"{
 value = aws_instance.cart.public_ip
+}
+resource "aws_route53_record" "cart" {
+  zone_id = "Z0458836EHR8MPWSFHZ5"
+  name    = "cart.devops1008.online"
+  type    = "A"
+  ttl     = "30"
+  records = [aws_instance.cart.private_ip]
 }
 
 resource "aws_instance" "mysql" {
@@ -81,6 +117,13 @@ resource "aws_instance" "mysql" {
 output "mysql"{
 value = aws_instance.mysql.public_ip
 }
+resource "aws_route53_record" "mysql" {
+  zone_id = "Z0458836EHR8MPWSFHZ5"
+  name    = "mysql.devops1008.online"
+  type    = "A"
+  ttl     = "30"
+  records = [aws_instance.mysql.private_ip]
+}
 
 resource "aws_instance" "shipping" {
   ami           = data.aws_ami.centos.image_id
@@ -93,6 +136,13 @@ resource "aws_instance" "shipping" {
 output "shipping"{
 value = aws_instance.shipping.public_ip
 }
+resource "aws_route53_record" "shipping" {
+  zone_id = "Z0458836EHR8MPWSFHZ5"
+  name    = "shipping.devops1008.online"
+  type    = "A"
+  ttl     = "30"
+  records = [aws_instance.shipping.private_ip]
+}
 
 resource "aws_instance" "rabbitmq" {
   ami           = data.aws_ami.centos.image_id
@@ -104,4 +154,11 @@ resource "aws_instance" "rabbitmq" {
 }
 output "rabbitmq"{
 value = aws_instance.rabbitmq.public_ip
+}
+resource "aws_route53_record" "rabbitmq" {
+  zone_id = "Z0458836EHR8MPWSFHZ5"
+  name    = "rabbitmq.devops1008.online"
+  type    = "A"
+  ttl     = "30"
+  records = [aws_instance.rabbitmq.private_ip]
 }
