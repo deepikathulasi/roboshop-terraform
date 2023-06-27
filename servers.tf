3f7627122cda91c354de57aab7feb71e1b9dd11e
Deepika <venkatadeepikathulasi@gmail.com>
2023-06-27 06:45:52 +0530

    terraform for-each

--- servers.tf
+++ servers.tf
@@ -82,12 +82,12 @@ Name=each.value["name"]
 }
 
 resource "aws_route53_record" "records"{
-for-each = var.components
-zone_id ="Z0458836EHR8MPWSFHZ5"
-name ="${each.value["name"]}-devops1008.online
-type="A"
-ttl=30
-records=[aws_instance.instance[each.value["name"]].private_ip]
+     for-each = var.components
+      zone_id ="Z0458836EHR8MPWSFHZ5"
+     name     ="${each.value["name"]}-devops1008.online"
+      type    ="A"
+      ttl     =30
+      records  =[aws_instance.instance[each.value["name"]].private_ip]
 }
 
 //output "frontend"{
