9bfd4aa40b572401884e6e0e192e1b99903e15b2
Deepika <venkatadeepikathulasi@gmail.com>
2023-07-05 08:45:25 +0530

    roboshop provisioner1

--- servers.tf
+++ servers.tf
@@ -30,7 +30,7 @@ provisioner "remote-exec" {
 resource "aws_route53_record" "records"{
      for_each = var.components
       zone_id ="Z0458836EHR8MPWSFHZ5"
-     name     ="${each.value["name"]}.devops1008.online"
+      name     ="${each.value["name"]}-dev.devops1008.online"
       type    ="A"
       ttl     =30
       records  =[aws_instance.instance[each.value["name"]].private_ip]
