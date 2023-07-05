802c0512e08cfd2fbffad27c3d8aa924fa044a6b
Deepika <venkatadeepikathulasi@gmail.com>
2023-07-05 07:36:43 +0530

    roboshop provisioner1

--- servers.tf
+++ servers.tf
@@ -19,7 +19,7 @@ host=self.private_ip
 }
 inline = [
 "rm-rf roboshop-shell",
-"git clone https://github.com/venkatadeepikathulasi/Roboshop-shell.git",
+"git clone https://github.com/deepikathulasi/roboshopshell.git,
 "cd Roboshop-shell",
 "sudo bash${each.value["name"]}.sh"]
 }
