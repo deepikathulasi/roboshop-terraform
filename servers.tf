fd5b1ce5bd5a5e42c0f1fa999a19714e6e9ca9c3
Deepika <venkatadeepikathulasi@gmail.com>
2023-07-05 07:49:16 +0530

    roboshop provisioner1

--- servers.tf
+++ servers.tf
@@ -1,6 +1,3 @@
-
-
-
 resource "aws_instance" "instance" {
 for_each=var.components
   ami           = data.aws_ami.centos.image_id
@@ -10,6 +7,9 @@ for_each=var.components
 tags={
 Name=each.value["name"]
 }
+resource "null_resource" "provisioner"{
+depends_on = [aws_instance,aws_route53_record.records]
+for_each                = var.components
 provisioner "remote-exec"{
 connection{
 type ="ssh"
@@ -18,12 +18,13 @@ password="DevOps321"
 host=self.private_ip
 }
 inline = [
-"rm-rf roboshop-shell",
-"git clone https://github.com/deepikathulasi/roboshopshell.git,
-"cd Roboshop-shell",
-"sudo bash${each.value["name"]}.sh"]
+  "rm-rf roboshop-shell",
+  "git clone https://github.com/deepikathulasi/roboshopshell.git,
+  "cd Roboshop-shell",
+  "sudo bash${each.value["name"]}.sh"
+]
 }
-
+}
 resource "aws_route53_record" "records"{
      for_each = var.components
       zone_id ="Z0458836EHR8MPWSFHZ5"
